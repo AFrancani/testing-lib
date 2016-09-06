852f40531daeaaa445d0b51345ddefaaf5e1425d
Allan Francani <afrancani@gmail.com>
2016-09-06 23:14:42 +0200

    moved the core logic of the tester into a TesterCore object

--- Tester.js
+++ Tester.js
@@ -1,16 +1,44 @@
 module.exports = function(logger) {
 	var colors = require('colors');
 
-	var beforeEachFunction = function() {};
+	var TesterCore = function() {
+		var beforeEachFunction = function() {};
+
+		function beforeEach(functionToRun) {
+			var previousBeforeEach = beforeEachFunction;
+			beforeEachFunction = function() {
+				previousBeforeEach();
+				functionToRun();
+			};
+		}
+
+		function testThat(name, functionContainingTest) {
+			beforeEachFunction();
+			try {
+				functionContainingTest();
+				return { name: name, passed: true };
+			} catch (e) {
+				return { name: name, passed: false, message: e.message };
+			}
+		}
+
+		function describe(name, functionContainingTests) {
+			var previousBeforeEach = beforeEachFunction;
+			functionContainingTests();
+			beforeEachFunction = previousBeforeEach;
+		}
+
+		return {
+			beforeEach: beforeEach,
+			testThat: testThat,
+			describe: describe
+		};
+	};
+
+	var testerCore = new TesterCore();
+
 	var prefixe = '';
 
-	function beforeEach(functionToRun) {
-		var previousBeforeEach = beforeEachFunction;
-		beforeEachFunction = function() {
-			previousBeforeEach();
-			functionToRun();
-		};
-	}
 
 	function addPrefixeSize(value) {
 		var size = prefixe.length + value;
@@ -25,7 +53,7 @@ module.exports = function(logger) {
 	}
 
 	function testThat(name, functionContainingTest) {
-		var testResult = runTest(name, functionContainingTest);
+		var testResult = testerCore.testThat(name, functionContainingTest);
 
 		log(testResult.name + (testResult.passed ? ' passed!'.green : ' failed!'.red));
 		if(!testResult.passed) {
@@ -35,29 +63,19 @@ module.exports = function(logger) {
 		return testResult.passed;
 	}
 
-	function runTest(name, functionContainingTest) {
-		beforeEachFunction();
-		try {
-			functionContainingTest();
-			return { name: name, passed: true };
-		} catch (e) {
-			return { name: name, passed: false, message: e.message };
-		}
-	}
-
 	function describe(name, functionContainingTests) {
-		var previousBeforeEach = beforeEachFunction;
 		log(' ');
 		log(name.bold);
 		addPrefixeSize(2);
-		functionContainingTests();
+
+		testerCore.describe(name, functionContainingTests);
+
 		addPrefixeSize(-2);
 		log(' ');
-		beforeEachFunction = previousBeforeEach;
 	}
 
 	return {
-		beforeEach: beforeEach,
+		beforeEach: testerCore.beforeEach,
 		testThat: testThat,
 		describe: describe
 	};
