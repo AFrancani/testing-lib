c1a6130b0cf6ef6cbe05a80e2a70e46448a0a99e
Allan Francani <afrancani@gmail.com>
2016-10-31 14:16:35 +0100

    added some describe to the existing tests

--- test/AssertionSpec.js
+++ test/AssertionSpec.js
@@ -1,44 +1,51 @@
 require('../testing-lib.js');
 
 describe('Assertion', function() {
-	testThat('fail should fail', function() {
-		try {
-			fail();
-			throw '';
-		} catch (e) {
-			if(!(e instanceof FailException)) {
-				throw new FailException();
-			}
-		}
-	});
 
-	testThat('isTrue should pass if value is true', function() {
-		isTrue(true);
-	});
-
-	testThat('isTrue should fail if value is false', function() {
-		try {
-			isTrue(false);
-			fail();
-		} catch (e) {
-			if(!(e instanceof TrueException)) {
+	describe('fail', function() {
+		testThat('it should fail', function() {
+			try {
 				fail();
+				throw '';
+			} catch (e) {
+				if(!(e instanceof FailException)) {
+					throw new FailException();
+				}
 			}
-		}
+		});
 	});
 
-	testThat('isFalse should pass if value is false', function() {
-		isFalse(false);
-	});
+	describe('isTrue', function() {
+		testThat('it should pass if value is true', function() {
+			isTrue(true);
+		});
 
-	testThat('isFalse should fail if value is true', function() {
-		try {
-			isFalse(true);
-			fail();
-		} catch (e) {
-			if(!(e instanceof FalseException)) {
+		testThat('it should fail if value is false', function() {
+			try {
+				isTrue(false);
 				fail();
+			} catch (e) {
+				if(!(e instanceof TrueException)) {
+					fail();
+				}
 			}
-		}
+		});
+	});
+
+	describe('isFalse', function() {
+		testThat('it should pass if value is false', function() {
+			isFalse(false);
+		});
+
+		testThat('it should fail if value is true', function() {
+			try {
+				isFalse(true);
+				fail();
+			} catch (e) {
+				if(!(e instanceof FalseException)) {
+					fail();
+				}
+			}
+		});
 	});
 });
--- test/LoggingTesterSpec.js
+++ test/LoggingTesterSpec.js
@@ -3,6 +3,7 @@ require('../testing-lib.js');
 describe('Logging tester', function() {
 	const FakeLogger =  require('./FakeLogger.js')
 		TesterFactory = require('../src/TesterFactory.js');
+		colors = require('colors');
 
 	var tester, logger;
 
@@ -11,82 +12,84 @@ describe('Logging tester', function() {
 		tester = TesterFactory.createLoggingTester({logger: logger});
 	});
 
-	var colors = require('colors');
-
-	testThat('testThat should log when test pass', function() {
-		tester.testThat('some test', function() {});
-
-		assertThat(logger.logged('V'.green + ' some test'), isTrue);
-	});
-
-	testThat('testThat should log when test fails', function() {
-		tester.testThat('some test', fail);
-
-		assertThat(logger.logged('X'.red + ' some test'), isTrue);
-	});
-
-	testThat('testThat should log when fail assertion fails', function() {
-		tester.testThat('some test', fail);
-
-		assertThat(logger.logged('  Test failed!'), isTrue);
-	});
-
-	testThat('testThat should log when true assertion fails', function() {
-		tester.testThat('some test', function() {
-			isTrue(false);
-		});
-
-		assertThat(logger.logged('  Given value is not true!'), isTrue);
-	});
-
-	testThat('testThat should log when false assertion fails', function() {
-		tester.testThat('some test', function() {
-			isFalse(true);
-		});
-
-		assertThat(logger.logged('  Given value is not false!'), isTrue);
-	});
-
-	testThat('testThat should log when equals assertion fails', function() {
-		tester.testThat('some test', function() {
-			equals('some text')(42);
-		});
-
-		assertThat(logger.logged('  42 does not equal some text'), isTrue);
-	});
-
-	testThat('describe should log the feature it describes', function() {
-		tester.describe('a feature', function() {
+	describe('testThat', function() {
+		testThat('it should log when test pass', function() {
 			tester.testThat('some test', function() {});
+
+			assertThat(logger.logged('V'.green + ' some test'), isTrue);
 		});
 
-		assertThat(logger.logged('a feature'.bold), isTrue);
+		testThat('it should log when test fails', function() {
+			tester.testThat('some test', fail);
+
+			assertThat(logger.logged('X'.red + ' some test'), isTrue);
+		});
+
+		testThat('it should log when fail assertion fails', function() {
+			tester.testThat('some test', fail);
+
+			assertThat(logger.logged('  Test failed!'), isTrue);
+		});
+
+		testThat('it should log when true assertion fails', function() {
+			tester.testThat('some test', function() {
+				isTrue(false);
+			});
+
+			assertThat(logger.logged('  Given value is not true!'), isTrue);
+		});
+
+		testThat('it should log when false assertion fails', function() {
+			tester.testThat('some test', function() {
+				isFalse(true);
+			});
+
+			assertThat(logger.logged('  Given value is not false!'), isTrue);
+		});
+
+		testThat('it should log when equals assertion fails', function() {
+			tester.testThat('some test', function() {
+				equals('some text')(42);
+			});
+
+			assertThat(logger.logged('  42 does not equal some text'), isTrue);
+		});
 	});
 
-	testThat('describe should add some space for the logger', function() {
-		tester.describe('a feature', function() {
-			tester.testThat('some test', function() {});
+	describe('describe', function() {
+		testThat('it should log the feature it describes', function() {
+			tester.describe('a feature', function() {
+				tester.testThat('some test', function() {});
+			});
+
+			assertThat(logger.logged('a feature'.bold), isTrue);
 		});
 
-		assertThat(logger.logged('  ' + 'V'.green + ' some test'), isTrue);
-	});
+		testThat('it should add some space for the logger', function() {
+			tester.describe('a feature', function() {
+				tester.testThat('some test', function() {});
+			});
 
-	testThat('describe should log the number of passed tests', function() {
-		tester.describe('a feature', function() {
-			tester.testThat('a test', function() {});
-			tester.testThat('a second test', function() {});
-			tester.testThat('a third test', function() {});
+			assertThat(logger.logged('  ' + 'V'.green + ' some test'), isTrue);
 		});
 
-		assertThat(logger.logged('  ' + '3 passing'.green), isTrue);
-	});
+		testThat('it should log the number of passed tests', function() {
+			tester.describe('a feature', function() {
+				tester.testThat('a test', function() {});
+				tester.testThat('a second test', function() {});
+				tester.testThat('a third test', function() {});
+			});
 
-	testThat('describe should log the number of failed tests', function() {
-		tester.describe('a feature', function() {
-			tester.testThat('a test', fail);
-			tester.testThat('a second test', fail);
+			assertThat(logger.logged('  ' + '3 passing'.green), isTrue);
 		});
 
-		assertThat(logger.logged('  ' + '2 failing'.red), isTrue);
+		testThat('it should log the number of failed tests', function() {
+			tester.describe('a feature', function() {
+				tester.testThat('a test', fail);
+				tester.testThat('a second test', fail);
+			});
+
+			assertThat(logger.logged('  ' + '2 failing'.red), isTrue);
+		});
 	});
 });
